cad481924c193425b6be977c52e3d45e50ea2f41
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-03-29 13:12:43 +0100

    Add license notice to program startup

--- src/main.c
+++ src/main.c
@@ -6,8 +6,18 @@
 
 #include <stdio.h>
 
+void print_license()
+{
+        printf("Polymine Copyright (C) 2024  Piotr K. Wyrwas\n"
+               "This program comes with ABSOLUTELY NO WARRANTY.\n"
+               "This is free software, and you are welcome to \nredistribute it "
+               "under the terms of the GNU GPL license\n\n");
+}
+
 int main(void)
 {
+        print_license();
+
         struct input_handle handle = empty_input_handle;
         if (!input_read("input.int", &handle)) {
                 printf("Could not load input file.\n");
@@ -17,8 +27,8 @@ int main(void)
         struct lexer lex;
         lexer_init(&lex, &handle);
 
-	struct parser p;
-	parser_init(&p, &lex);
+        struct parser p;
+        parser_init(&p, &lex);
 
         struct astnode *node = parse(&p);
         if (node) {
